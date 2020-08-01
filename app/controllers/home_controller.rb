815e17582211672fec5eeca4e8d333df1fa0a0d0
Eduardo Henrique Gris <gris.eduardo@gmail.com>
2020-08-01 18:09:57 -0300

    Receitas na index com Ruby vanilla

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @recipes = []
+    @recipes << Recipe.new(title: 'Bolo', cuisine: 'Brasileira', 
+                           recipe_type: 'Sobremesa', cook_time: 30)
+    @recipes << Recipe.new(title: 'Bolo de cenoura', cuisine: 'Brasileira', 
+                           recipe_type: 'Sobremesa', cook_time: 30)
   end
 end
